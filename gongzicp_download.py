50666a9820b90c432ede914b914b1c11efe78e64
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-05-30 15:27:06 -0700

    commit

--- gongzicp_download.py
+++ gongzicp_download.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 import yaml
 import time
 import os
+from telegram_util import compactText
 
 chapter_prefix = 'https://www.gongzicp.com/novel/getChapterList?nid='
 detail_prefix = 'https://www.gongzicp.com/read-%s.html'
@@ -20,29 +21,35 @@ def getIds(content):
 		if cid:
 			yield cid
 
-def download(url):
+def getContent(raw_content):
+	content = raw_content.split(content_anchor)[1].split('",')[0]
+	soup = BeautifulSoup(content.replace('\/', '/'), 'html.parser')
+	for item in soup.find_all('p'):
+		if 'hidden' in str(item.attrs):
+			item.decompose()
+	return soup.text.encode().decode('unicode-escape')
+
+def getNovelName(raw_content):
+	novel_name = raw_content.split(novel_anchor)[1].split('",')[0]
+	return novel_name.encode().decode('unicode-escape')
+
+def download(url, force_cache = False):
 	nid = getNid(url)
-	content = cached_url.get(chapter_prefix + nid)
+	content = cached_url.get(chapter_prefix + nid, force_cache = force_cache)
 	content = yaml.load(content, Loader=yaml.FullLoader)
 	novel_name = None
+	result = []
 	for cid in getIds(content):
-		raw_content = cached_url.get(detail_prefix % cid)
-		raw_content = raw_content
-		content = raw_content.split(content_anchor)[1].split('",')[0]
+		raw_content = cached_url.get(
+			detail_prefix % cid, 
+			force_cache = force_cache,
+			sleep = 1)
 		if not novel_name:
-			novel_name = raw_content.split(novel_anchor)[1].split('",')[0]
-			novel_name = novel_name.encode().decode('unicode-escape')
+			novel_name = getNovelName(raw_content)
 			os.system('mkdir download > /dev/null 2>&1')
-			os.system('rm download/%s.txt > /dev/null 2>&1' % novel_name)
-		content = content.replace('\/', '/')
-		soup = BeautifulSoup(content, 'html.parser')
-		for item in soup.find_all('p', class_='cp-hidden'):
-			print(item)
-			item.decompose()
-		with open('download/%s.txt' % novel_name, 'a') as f:
-			f.write(soup.text.encode().decode('unicode-escape'))
-		time.sleep(1)
-		return
+		result.append(getContent(raw_content))
+	with open('download/%s.txt' % novel_name, 'w') as f:
+		f.write(compactText(''.join(result)))
 	
 if __name__ == "__main__":
-	download('https://www.gongzicp.com/novel-168140.html')
+	download('https://www.gongzicp.com/novel-168140.html', force_cache = True)
